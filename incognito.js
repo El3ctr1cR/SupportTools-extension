96106ef80cd54ab6e2311b9976623fa95d1e0363
El3ctr1cR <ruben.ruitenberg@hotmail.com>
2024-08-08 12:14:25 +0200

    Fixed a bug where  incognito would open and close

--- incognito.js
+++ incognito.js
@@ -1,5 +1,9 @@
+let isTabHandled = {};
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'loading' && tab.url) {
+    if (isTabHandled[tabId]) return;
+
     chrome.storage.sync.get(['bypassIncognito'], (result) => {
       const bypassIncognito = result.bypassIncognito || false;
 
@@ -13,14 +17,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
           chrome.tabs.update(tabId, { url: newUrl });
         } else if (hostname.includes('admin.microsoft.com') ||
                    hostname.includes('portal.azure.com')) {
+          isTabHandled[tabId] = true;
+
           chrome.windows.create({
             url: tab.url,
             incognito: true
           }, () => {
-            chrome.tabs.remove(tabId);
+            chrome.tabs.remove(tabId, () => {
+              delete isTabHandled[tabId];
+            });
           });
         }
       }
     });
   }
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  delete isTabHandled[tabId];
 });
